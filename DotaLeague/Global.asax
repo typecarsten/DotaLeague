f6c4a11f88bc9625efbb1b8c6a5520178e356f4b
Carsten Jacobsen <callefix91@gmail.com>
2014-07-05 10:46:32 +0200

    Added startup project

---
+++ DotaLeague/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotaLeague.MvcApplication" Language="C#" %>
---
+++ DotaLeague/Startup.cs
@@ -0,0 +1,15 @@
+﻿using DotaLeague;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace DotaLeague
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
