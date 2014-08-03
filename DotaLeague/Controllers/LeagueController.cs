47406b1aa8eebc5dd6bae833d2f71d4278249d82
Carsten Jacobsen <callefix91@gmail.com>
2014-08-03 23:32:53 +0200

    Nuget updates and owin added for steam support

---
+++ DotaLeague/Controllers/LeagueController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DotaLeague.Models;
+using DotaLeague.DAL;
+
+namespace DotaLeague.Controllers
+{
+    public class LeagueController : Controller
+    {
+        private WebpageContext db = new WebpageContext();
+
+        // GET: /League/
+        public ActionResult Index()
+        {
+            return View(db.League.ToList());
+        }
+
+        // GET: /League/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            League league = db.League.Find(id);
+            if (league == null)
+            {
+                return HttpNotFound();
+            }
+            return View(league);
+        }
+
+        // GET: /League/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /League/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="LeagueID,LeagueName,Description,NumberOfTeams")] League league)
+        {
+            if (ModelState.IsValid)
+            {
+                db.League.Add(league);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(league);
+        }
+
+        // GET: /League/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            League league = db.League.Find(id);
+            if (league == null)
+            {
+                return HttpNotFound();
+            }
+            return View(league);
+        }
+
+        // POST: /League/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="LeagueID,LeagueName,Description,NumberOfTeams")] League league)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(league).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(league);
+        }
+
+        // GET: /League/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            League league = db.League.Find(id);
+            if (league == null)
+            {
+                return HttpNotFound();
+            }
+            return View(league);
+        }
+
+        // POST: /League/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            League league = db.League.Find(id);
+            db.League.Remove(league);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
